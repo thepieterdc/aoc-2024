718d1a34cf5c9f32ab4e6301c25a27102b488271
Pieter De Clercq <pieter.de.clercq@deliverect.com>
2024-12-04 12:43:05 +0100

    Cleanup

--- Day4/Simple.hs
+++ Day4/Simple.hs
@@ -8,65 +8,41 @@ data Match = N | NE | E | SE | S | SW | W | NW deriving (Eq, Show)
 type Grid = [String]
 
 findMas :: Int -> Int -> Grid -> Int
-findMas xr xc g = length $ catMaybes [findN, findNE, findE, findSE, findS, findSW, findW, findNW]
+findMas xr xc g = length $ filter (\x -> map char x == "MAS") [findN, findNE, findE, findSE, findS, findSW, findW, findNW]
   where
-    char r' c' = if r' >= 0 && r' < length g && c' >= 0 && c' < length (g !! r') then g !! r' !! c' else '.'
+    char (r', c') = if r' >= 0 && r' < length g && c' >= 0 && c' < length (g !! r') then g !! r' !! c' else '.'
     findN =
-      if "MAS"
-        == [ char r c
-             | (r, c) <- [(xr, xc - 1), (xr, xc - 2), (xr, xc - 3)]
-           ]
-        then Just N
-        else Nothing
+      [ (r, c)
+        | (r, c) <- [(xr, xc - 1), (xr, xc - 2), (xr, xc - 3)]
+      ]
     findNE =
-      if "MAS"
-        == [ char r c
-             | (r, c) <- [(xr - 1, xc + 1), (xr - 2, xc + 2), (xr - 3, xc + 3)]
-           ]
-        then Just NE
-        else Nothing
+      [ (r, c)
+        | (r, c) <- [(xr - 1, xc + 1), (xr - 2, xc + 2), (xr - 3, xc + 3)]
+      ]
     findE =
-      if "MAS"
-        == [ char r c
-             | (r, c) <- [(xr, xc + 1), (xr, xc + 2), (xr, xc + 3)]
-           ]
-        then Just E
-        else Nothing
+      [ (r, c)
+        | (r, c) <- [(xr, xc + 1), (xr, xc + 2), (xr, xc + 3)]
+      ]
     findSE =
-      if "MAS"
-        == [ char r c
-             | (r, c) <- [(xr + 1, xc + 1), (xr + 2, xc + 2), (xr + 3, xc + 3)]
-           ]
-        then Just SE
-        else Nothing
+      [ (r, c)
+        | (r, c) <- [(xr + 1, xc + 1), (xr + 2, xc + 2), (xr + 3, xc + 3)]
+      ]
     findS =
-      if "MAS"
-        == [ char r c
-             | (r, c) <- [(xr + 1, xc), (xr + 2, xc), (xr + 3, xc)]
-           ]
-        then Just S
-        else Nothing
+      [ (r, c)
+        | (r, c) <- [(xr + 1, xc), (xr + 2, xc), (xr + 3, xc)]
+      ]
     findSW =
-      if "MAS"
-        == [ char r c
-             | (r, c) <- [(xr + 1, xc - 1), (xr + 2, xc - 2), (xr + 3, xc - 3)]
-           ]
-        then Just SW
-        else Nothing
+      [ (r, c)
+        | (r, c) <- [(xr + 1, xc - 1), (xr + 2, xc - 2), (xr + 3, xc - 3)]
+      ]
     findW =
-      if "MAS"
-        == [ char r c
-             | (r, c) <- [(xr, xc - 1), (xr, xc - 2), (xr, xc - 3)]
-           ]
-        then Just W
-        else Nothing
+      [ (r, c)
+        | (r, c) <- [(xr, xc - 1), (xr, xc - 2), (xr, xc - 3)]
+      ]
     findNW =
-      if "MAS"
-        == [ char r c
-             | (r, c) <- [(xr - 1, xc - 1), (xr - 2, xc - 2), (xr - 3, xc - 3)]
-           ]
-        then Just NW
-        else Nothing
+      [ (r, c)
+        | (r, c) <- [(xr - 1, xc - 1), (xr - 2, xc - 2), (xr - 3, xc - 3)]
+      ]
 
 findXmas :: Grid -> Int
 findXmas g = sum [findMas r c g | r <- [0 .. length g - 1], c <- [0 .. length (g !! r) - 1], g !! r !! c == 'X']
