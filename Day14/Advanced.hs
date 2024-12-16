f700528cd81abcd14a7b252548952647fc88ef02
Pieter De Clercq <pieter.de.clercq@deliverect.com>
2024-12-16 15:07:00 +0100

    Cleanup

--- Day14/Advanced.hs
+++ Day14/Advanced.hs
@@ -1,12 +1,12 @@
 module Day14.Advanced where
 
-import qualified Data.IntMap as Set
+import Data.List (nub)
 import Day14.Common (Puzzle (..), move, parse)
 import Utils.IO (loadInput)
 import Utils.Parser (doParse)
 
 isXmasTree :: [(Int, Int)] -> Bool
-isXmasTree robots = length robots == Set.size (Set.fromList robots)
+isXmasTree robots = length robots == length (nub robots)
 
 run :: Int -> Puzzle -> Int
 run i (Puzzle robots w h)
